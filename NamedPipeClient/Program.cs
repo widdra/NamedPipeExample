2c52b0ce1a5104c06c907cd8f1a38c023fd98fe0
philippwiddra <philipp.widdra@gmail.com>
2016-08-30 23:42:09 +0200

    Initial commit

---
+++ NamedPipeClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace NamedPipeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pipeFactory = new ChannelFactory<IMessageDisplayer>(new NetNamedPipeBinding(), new EndpointAddress("net.pipe://localhost/Reverse"));
+            var pipeProxy = pipeFactory.CreateChannel();
+
+            Console.WriteLine("Client started. Type ahead. Enter blank line to exit.");
+
+            while (true)
+            {
+                var input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Enter) break;
+
+                pipeProxy.DisplayCharacter(input.KeyChar);
+            }
+        }
+
+        [ServiceContract]
+        public interface IMessageDisplayer
+        {
+            [OperationContract]
+            void DisplayMessage(string value);
+
+            [OperationContract]
+            void DisplayCharacter(char value);
+        }
+    }
+}
---
+++ NamedPipeServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedPipeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var serviceHost = new ServiceHost(typeof(MessageDisplayer), new Uri("net.pipe://localhost")))
+            {
+                serviceHost.AddServiceEndpoint(typeof(IMessageDisplayer), new NetNamedPipeBinding(), "Reverse");
+                serviceHost.Open();
+
+                Console.WriteLine("Service is available. Press any key to exit.");
+                Console.ReadKey();
+
+                serviceHost.Close();
+            }
+        }
+
+        [ServiceContract]
+        public interface IMessageDisplayer
+        {
+            [OperationContract]
+            void DisplayMessage(string value);
+
+            [OperationContract]
+            void DisplayCharacter(char value);
+        }
+
+        public class MessageDisplayer : IMessageDisplayer
+        {
+            public void DisplayMessage(string value)
+            {
+                Console.WriteLine(value);
+            }
+
+            public void DisplayCharacter(char value)
+            {
+                Console.Write(value);
+            }
+        }
+    }
+}
